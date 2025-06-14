6930e45a88401c29e85aa35f3bfcc32af144740f
whyaftab7868 <ainstaagram@gmail.com>
2025-06-14 16:07:59 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'WHYAFTAB',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "ASAD-MD",
+  botOwner: 'WHYAFTAB', // Your name
+  ownerNumber: '92013278551', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
